8c144f23112a1374ca1ad6e80bceeff342558cc1
grepgiron <gregoriopaguada@outlook.com>
2017-06-28 11:59:49 -0600

    sqlite, tables

--- app/src/main/java/irama/irama/Sqlite/DBHelper.java
+++ app/src/main/java/irama/irama/Sqlite/DBHelper.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import irama.irama.Sqlite.Tables.feed;
+import irama.irama.Sqlite.Tables.feedSqlite;
 
 /**
  * Created by enagi on 28/6/2017.
@@ -22,13 +22,17 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(feed.feedEntry.SQL_CREATE_CLIENTS);
+        db.execSQL(feedSqlite.feedClient.SQL_CREATE_CLIENTS);
+        db.execSQL(feedSqlite.feedProductCategory.SQL_CREATE_CATEGORY);
+        db.execSQL(feedSqlite.feedOrder.SQL_CREATE_ORDER);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL(feed.feedEntry.SQL_DROP_CLIENTS);
+        db.execSQL(feedSqlite.feedClient.SQL_DROP_CLIENTS);
+        db.execSQL(feedSqlite.feedProductCategory.SQL_DROP_CATEGORY);
+        db.execSQL(feedSqlite.feedOrder.SQL_DROP_ORDER);
         onCreate(db);
     }
 }
---
+++ app/src/main/java/irama/irama/Sqlite/Tables/feedSqlite.java
@@ -0,0 +1,95 @@
+package irama.irama.Sqlite.Tables;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by enagi on 28/6/2017.
+ */
+
+public final class feedSqlite {
+    private feedSqlite() {
+    }
+
+    public static class feedClient implements BaseColumns{
+
+        private static final String TABLE_NAME = "client";
+        private static final String COLUMN_CLIENT_CODE = "code";
+        private static final String COLUMN_CLIENT_NAME = "name";
+        private static final String COLUMN_CLIENT_RTN = "rtn";
+        private static final String COLUMN_CLIENT_ACTIVE = "active";
+
+        private static final String TEXT_TYPE = " TEXT";
+        private static final String BOOLEAN_TYPE = " BOOLEAN";
+        private static final String COMMA_SEP = ",";
+
+        public static final String SQL_CREATE_CLIENTS =
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        _ID + " INTEGER PRIMARY KEY," +
+                        COLUMN_CLIENT_NAME + TEXT_TYPE + COMMA_SEP +
+                        COLUMN_CLIENT_RTN + TEXT_TYPE + COMMA_SEP +
+                        COLUMN_CLIENT_CODE + TEXT_TYPE + COMMA_SEP +
+                        COLUMN_CLIENT_ACTIVE + BOOLEAN_TYPE + " )";
+
+        public static final String SQL_DROP_CLIENTS =
+                "DROP TABLE IF EXISTS " + TABLE_NAME;
+    }
+
+    public static class feedProductCategory implements BaseColumns{
+
+        private static final String TABLE_NAME = "product_category";
+        private static final String COLUMN_CATEGORY_ID = "_id";
+        private static final String COLUMN_CATEGORY_NAME = "name";
+        private static final String COLUMN_CATEGORY_DESCRIPTION = "description";
+
+        private static final String TEXT_TYPE = " TEXT";
+        private static final String COMMA_SEP = ",";
+
+        public static final String SQL_CREATE_CATEGORY =
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        _ID + " INTEGER PRIMARY KEY," +
+                        COLUMN_CATEGORY_ID + TEXT_TYPE + COMMA_SEP +
+                        COLUMN_CATEGORY_NAME + TEXT_TYPE + COMMA_SEP +
+                        COLUMN_CATEGORY_DESCRIPTION + TEXT_TYPE + " )";
+
+        public static final String SQL_DROP_CATEGORY =
+                "DROP IF EXISTS " + TABLE_NAME;
+    }
+
+    public static final class feedOrder implements BaseColumns{
+
+        private static final String TABLE_NAME = "product_category";
+        private static final String COLUMN_ORDER_ID = "_id";
+        private static final String COLUMN_ORDER_DESCRIPTION = "description";
+        private static final String COLUMN_ORDER_URGENT = "isUrgent";
+        private static final String COLUMN_ORDER_REQUEST_ON = "requestOn";
+        private static final String COLUMN_ORDER_CODE = "code";
+        private static final String COLUMN_ORDER_CLIENT_ID = "clientId";
+        private static final String COLUMN_ORDER_PRODUCT_ID = "productId";
+        private static final String COLUMN_ORDER_EMPLOYEE_ID = "employeeId";
+        private static final String COLUMN_ORDER_SERIE_ID = "serieId";
+        private static final String COLUMN_ORDER_ORDER_TYPE = "orderTypetId";
+
+        private static final String TEXT_TYPE = " TEXT";
+        private static final String BOOLEAN_TYPE = " BOOLEAN";
+        private static final String COMMA_SEP = ",";
+
+        public static final String SQL_CREATE_ORDER =
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        _ID + " INTEGER PRIMARY KEY," +
+                        COLUMN_ORDER_ID + TEXT_TYPE + COMMA_SEP +
+                        COLUMN_ORDER_DESCRIPTION + TEXT_TYPE + COMMA_SEP +
+                        COLUMN_ORDER_REQUEST_ON + TEXT_TYPE + COMMA_SEP +
+                        COLUMN_ORDER_CODE + TEXT_TYPE + COMMA_SEP +
+                        COLUMN_ORDER_CLIENT_ID + TEXT_TYPE + COMMA_SEP +
+                        COLUMN_ORDER_PRODUCT_ID + TEXT_TYPE + COMMA_SEP +
+                        COLUMN_ORDER_EMPLOYEE_ID + TEXT_TYPE + COMMA_SEP +
+                        COLUMN_ORDER_SERIE_ID + TEXT_TYPE + COMMA_SEP +
+                        COLUMN_ORDER_ORDER_TYPE + TEXT_TYPE + COMMA_SEP +
+                        COLUMN_ORDER_URGENT + BOOLEAN_TYPE + " )";
+
+        public static final String SQL_DROP_ORDER =
+                "DROP IF EXISTS " + TABLE_NAME;
+
+    }
+
+}
